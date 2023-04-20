c40115808eb81f3b181a17288db95e8508274639
Carmonel <zhurkovjaroslav@gmail.com>
2023-04-20 20:45:27 +0300

    Added Matrix family from Lab 5.

--- src/Lab8/Main.java
+++ src/Lab8/Main.java
@@ -1,8 +1,9 @@
 package Lab8;
 import Lab8.matrix.*;
-
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ArrayList<Integer> b;
     }
 }
