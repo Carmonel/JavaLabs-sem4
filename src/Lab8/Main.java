7681f6f4ae115116b2bdbfec4746a90015c6dd22
Carmonel <zhurkovjaroslav@gmail.com>
2023-04-22 12:27:27 +0300

    Checking PUSH on the laptop

--- src/Lab8/Main.java
+++ src/Lab8/Main.java
@@ -3,7 +3,7 @@ import Lab8.matrix.*;
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! 1");
         ArrayList<Integer> b;
     }
 }
