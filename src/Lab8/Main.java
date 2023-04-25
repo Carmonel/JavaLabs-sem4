89dd4c41a92e10348dc50cec0bd653b3346ba92a
Carmonel <zhurkovjaroslav@gmail.com>
2023-04-25 14:05:11 +0300

    Deleted genetal main.

--- src/Lab8/Main.java
+++
@@ -1,9 +0,0 @@
-package Lab8;
-import Lab8.matrix.*;
-import java.util.ArrayList;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world! 1");
-        ArrayList<Integer> b;
-    }
-}
